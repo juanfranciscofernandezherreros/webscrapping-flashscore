ad2fb98f9165d43359bf5bc352a068f427f35856
Your Name <juanfranciscofernandezherreros>
2023-03-29 23:49:56 +0200

    Add to matchs

--- readCsvWithDifferentsName3.py
+++ readCsvWithDifferentsName3.py
@@ -11,9 +11,7 @@ db = mysql.connector.connect(
 )
 
 def read_csv_files(csv_files):
-    
     all_data = []
-    processed_rows = set() # To keep track of already processed rows
     
     for file in csv_files:
         print("File" + file)
@@ -21,10 +19,7 @@ def read_csv_files(csv_files):
             reader = csv.reader(csvfile, delimiter=';', quotechar='"')
             next(reader) # Skip header row
             for row in reader:
-                row_tuple = tuple(row) # Convert row to tuple for set comparison
-                if row_tuple not in processed_rows: # Check if row is not already processed
-                    all_data.append(row) # Add row to all_data
-                    processed_rows.add(row_tuple) # Add row to processed rows set
+                all_data.append(row) # Add row to all_data                
     
     # Prepare SQL statement
     sql = "INSERT INTO matchs (EventTimeUTC, EventTime, HomeTeam, AwayTeam, Quarter1Home, Quarter2Home, Quarter3Home, Quarter4Home, OvertimeHome, Quarter1Away, Quarter2Away, Quarter3Away, Quarter4Away, OvertimeAway, matchId) \
