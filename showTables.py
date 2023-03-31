66b5e6f5ede6c6bec92bfe25aa323b8d57849119
Your Name <juanfranciscofernandezherreros>
2023-03-31 11:57:45 +0200

    contar elementos que hay en la tablas

--- showTables.py
+++ showTables.py
@@ -1,22 +1,21 @@
 import mysql.connector
-
-# Conectarse a la base de datos
-db = mysql.connector.connect(
-    host="localhost",
-    user="user_bigdataetl",
-    password="password_bigdataetl",
-    database="bigdataetl"
-)
+from database import conectar
+# Obtener la conexión a la base de datos
+db = conectar()
 
 # Crear un cursor para ejecutar consultas SQL
 cursor = db.cursor()
 
-# Ejecutar una consulta SQL para obtener el número de filas de cada tabla
+# Ejecutar una consulta SQL
 cursor.execute("SELECT table_name, table_rows FROM information_schema.tables WHERE table_schema = 'bigdataetl'")
 
 # Recuperar los resultados de la consulta
 resultados = cursor.fetchall()
 
-# Imprimir el nombre de cada tabla y el número de filas que tiene
 for tabla in resultados:
     print(f"{tabla[0]} tiene {tabla[1]} filas")
+
+# Cerrar la conexión a la base de datos
+db.close()
+
+
