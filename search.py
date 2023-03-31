19336171324e7c3355589a00c97243517ab20925
Your Name <juanfranciscofernandezherreros>
2023-03-31 13:10:19 +0200

    contar los errores que hay

--- search.py
+++ search.py
@@ -3,8 +3,8 @@ import asyncio
 import csv
 import mysql.connector
 from pyppeteer import launch
-#py searchFinal.py https://www.flashscore.com/basketball/spain/
-async def main(url):
+
+async def main():
 
     # Connect to the MySQL server
     db = mysql.connector.connect(
@@ -14,7 +14,7 @@ async def main(url):
         database="bigdataetl"
     )
     
-    #Get list of emails to send
+    #Get list of urls to open
     # Create a cursor object
     mycursor = db.cursor()
 
@@ -27,69 +27,70 @@ async def main(url):
     # Get the results
     results = mycursor.fetchall()
     
-     # Check if the result is not empty
-
     # Check if the result is not empty
     if len(results) > 0:
         # Iterate over the results
         print("Len: " + str(len(results)))
         for row in results:
-                email = row[0]
-                # Do something with the email                
-    
-    browser = await launch(headless=True)
-    page = await browser.newPage()
-    await page.goto(url)
-       
-    print("URL" + url)    
+            url = row[0]
+            # Do something with the url
 
-    # Prepare SQL statement
-    update_sql = "UPDATE urls SET isOpened = 'T' WHERE urls = '"+url+"'"
-    
-    print("Update SQL" + update_sql)
-    # Execute the update statement
-    cursor = db.cursor()
+            browser = await launch(headless=True)
+            page = await browser.newPage()
+            await page.goto(url)
 
-    try:
-        cursor.execute(update_sql)
-        db.commit()
-    except mysql.connector.Error as error:
-        db.rollback()
-        print("Error updating row: {}".format(error))
-    finally:
-        cursor.close()
+            print("URL: " + url)
 
-    
-    hrefs = []
-    links = await page.querySelectorAll('a')
-    for link in links:
-        href = await page.evaluate('(element) => element.href', link)
-        if href:
-            hrefs.append(href)
-    
-    await browser.close()
-    
-    filtered_hrefs = set(filter(lambda x: 'basketball' in x and x.count('/') in [5, 6, 7], hrefs))
-    
-    # Prepare SQL statement
-    sql = "INSERT INTO urls (urls, isOpened) VALUES (%s, 'F')" 
-    
-    for row in filtered_hrefs:
-        values = (row,)
-        cursor = db.cursor()
-        
-        try:
-            print(sql)
-            cursor.execute(sql, values)
-            db.commit()            
-        except mysql.connector.Error as error:
-            db.rollback()
-            print("Error inserting row {}: {}".format(row, error))
-        finally:
-            cursor.close()  
+            # Prepare SQL statement
+            update_sql = "UPDATE urls SET isOpened = 'T' WHERE urls = '"+url+"'"
+            
+            print("Update SQL: " + update_sql)
+
+            # Execute the update statement
+            cursor = db.cursor()
+
+            try:
+                cursor.execute(update_sql)
+                db.commit()
+            except mysql.connector.Error as error:
+                db.rollback()
+                print("Error updating row: {}".format(error))
+            finally:
+                cursor.close()
+
+            hrefs = []
+            links = await page.querySelectorAll('a')
+            for link in links:
+                href = await page.evaluate('(element) => element.href', link)
+                if href:
+                    hrefs.append(href)
+
+            await browser.close()
+
+            filtered_hrefs = set(filter(lambda x: 'basketball' in x and x.count('/') in [5, 6, 7], hrefs))
+
+            # Prepare SQL statement
+            sql = "INSERT INTO urls (urls, isOpened) VALUES (%s, 'F')"
+            
+            errors = []
+
+            for row in filtered_hrefs:
+                values = (row,)
+                cursor = db.cursor()
+                
+                try:
+                    cursor.execute(sql, values)
+                    db.commit()            
+                except mysql.connector.Error as error:
+                    db.rollback()
+                    error_msg = "Error inserting row {}: {}".format(row, error)
+                    errors.append(error_msg)
+                finally:
+                    cursor.close()
+
+            # Count the number of errors and print them
+            num_errors = sum(1 for error in errors if error)
+            print("Number of errors: {}".format(num_errors))   
             
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extract all hrefs from a web page')
-    parser.add_argument('url', type=str, help='URL of the web page')
-    args = parser.parse_args()
-    asyncio.get_event_loop().run_until_complete(main(args.url))
+    asyncio.get_event_loop().run_until_complete(main())
