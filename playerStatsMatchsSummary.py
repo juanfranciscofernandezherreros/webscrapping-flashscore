184de342681bcbf9e1fd7e40b97bbf1b54db10f2
Your Name <juanfranciscofernandezherreros>
2023-03-31 23:30:01 +0200

    quarters and sumary

--- playerStatsMatchsSummary.py
+++ playerStatsMatchsSummary.py
@@ -2,9 +2,25 @@ import asyncio
 import csv
 import sys
 from pyppeteer import launch
+import os
 
 
 async def main(url):
+
+    # Make sure the "csv" folder exists
+    if not os.path.exists("csv"):
+        os.mkdir("csv")
+
+    # Create a subfolder called "basketball"
+    basketball_folder = os.path.join("csv", "basketball")
+    if not os.path.exists(basketball_folder):
+        os.mkdir(basketball_folder)
+
+    # Create a subfolder called "summary"
+    urls_folder = os.path.join(basketball_folder, "summary")
+    if not os.path.exists(urls_folder):
+        os.mkdir(urls_folder)
+    
     browser = await launch(headless=False)
     page = await browser.newPage()
     await page.goto(url)
@@ -51,7 +67,7 @@ async def main(url):
 
     # Write data to CSV file
     match_id = url.split("/")[4]
-    with open(f'match_data_summary_{match_id}.csv', 'w', newline='') as csvfile:
+    with open(f'csv/basketball/summary/summary_{match_id}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['ID', 'Name', 'Link', 'Stat1', 'Stat2', 'Stat3', 'Stat4', 'Stat5', 'Stat6', 'Stat7', 'Stat8', 'Stat9', 'Stat10', 'Stat11', 'Stat12', 'Stat13', 'Stat14'])
         for player_data in data:
