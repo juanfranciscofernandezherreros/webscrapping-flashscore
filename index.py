5d5cba1b27fb27d46cbd6c454644a8c59223a17f
Your Name <juanfranciscofernandezherreros>
2023-04-05 22:56:35 +0200

    creando un control de errores

--- index.py
+++ index.py
@@ -6,10 +6,10 @@ import requests
 import time
 import mysql.connector
 import asyncio
-import argparse
 import datetime
+import os
 from pyppeteer import launch
-
+import sys
 
 def consulta_count(mycursor):
     # Generate the SELECT statement
@@ -36,6 +36,14 @@ async def scrape_url(url):
     return hrefs
 
 async def main():
+
+        # Contadores para errores y aciertos
+        inserted_count = 0
+        error_count = 0
+
+        # Nombre del archivo python
+        filename = sys.argv[0]
+        
         # Conecta a la base de datos
         mydb = mysql.connector.connect(
             host="localhost",
@@ -63,9 +71,35 @@ async def main():
         count = consulta_count(mycursor)
         print("Init Count")
         print(len(count))
+        
+        # Nombre del archivo python ejecutado
+        filename = os.path.basename(__file__)
+
+        # Contadores
+        inserted_count = 0
+        error_count = 0
+        
+
+        
         # Realiza la consulta count en la URL
-        if len(count) > 0:                        
+        if len(count) > 0:          
+                        
+            # Make sure the "csv" folder exists
+            if not os.path.exists("csv"):
+                os.mkdir("csv")
+
+            # Create a subfolder called "basketball"
+            basketball_folder = os.path.join("csv", "basketball")
+            if not os.path.exists(basketball_folder):
+                os.mkdir(basketball_folder)
+
+            # Create a subfolder called "errors_events"
+            urls_folder = os.path.join(basketball_folder, "errors_events")
+            if not os.path.exists(urls_folder):
+                os.mkdir(urls_folder)
+        
             opened_urls_count = 0
+            
             for row in count:
                 url = row[0]
                 # Do something with the url
@@ -106,13 +140,8 @@ async def main():
                     mydb.rollback()
                 finally:
                     cursor.close()                              
-                
-                # Get current date and time
-                timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-                # Construct file name with timestamp
-                filename = f"urls_{timestamp}"
-                                  
+                  
                 # Filter and store the urls
                 filtered_hrefs = set(filter(lambda x: 'basketball' in x and x.count('/') in [5, 6, 7], hrefs))
                 sql = "INSERT INTO urls (urls, isOpened) VALUES (%s, 'F')"
@@ -128,23 +157,48 @@ async def main():
                         cursor.execute(sql, values)
                         mydb.commit()
                         inserted_count += 1
-                    except mysql.connector.errors.IntegrityError as error:
+                    except mysql.connector.errors.IntegrityError as error:                        
                         if "Duplicate entry" not in str(error):
                             error_rows.append({"error": str(error)})
+
                         mydb.rollback()
                     finally:
                         cursor.close()
+                    
                 count = consulta_count(mycursor)
+                
+                print("Inserciones exitosas:", inserted_count)
+                print("Inserciones fallidas:", error_count)
                 print(" Count")
                 print(len(count))
+                
+                # Inserción de errores
+                if(error_count > 0):
+                    error_cursor = mydb.cursor()
+                    error_values = (filename, str(inserted_count) , str(error_count))
+                    error_sql = "INSERT INTO errors_events (nameScriptPython, success,errors) VALUES (%s, %s,%s)"
+                    error_cursor.execute(error_sql, error_values)
+                                    
+                    # Get current date and time
+                    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+                    # Construct file name with timestamp
+                    filename = f"{sys.argv[0]}_{timestamp}"
+                    
+                    # Write error_rows to CSV file
+                    with open(f"csv/basketball/errors_events/{filename}", mode="w", newline="") as csvfile:
+                        fieldnames = ["error"]
+                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                     
                 if len(count) == 0:                        
                     break
 
         time.sleep(1)
-
+        
         
         # Cierra la conexin a la base de datos
         mydb.close()
+
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
--- init-db/errors_events.sql
+++ init-db/errors_events.sql
@@ -1,7 +1,6 @@
 CREATE TABLE errors_events (
     id BIGINT NOT NULL AUTO_INCREMENT,
 	nameScriptPython VARCHAR(200),
-	nameCsvWithErrors VARCHAR(200),	
 	success VARCHAR(200),	
 	errors VARCHAR(200),
     PRIMARY KEY (id)
