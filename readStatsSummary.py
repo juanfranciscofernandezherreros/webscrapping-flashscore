05a1d5d942fb4601988ef761805e0fbfa24f4659
Your Name <juanfranciscofernandezherreros>
2023-04-10 21:01:27 +0200

    stats summary

--- readStatsSummary.py
+++ readStatsSummary.py
@@ -58,13 +58,22 @@ def read_csv_files(csv_files):
                 # Split the row into parts using ',' as a separator
                 parts1 = row[0]
                 parts2 = row[1]
-                
-                all_data.append([parts1, parts2]) # Add row to all_data                
+                parts3 = row[2]
+                parts4 = row[3]
+                parts5 = row[4]
+                parts6 = row[5]
+                parts7 = row[6]
+                parts8 = row[7]
+                parts9 = row[8]
+                parts10 = row[9]
+                parts11 = row[10]
+                parts12 = row[11]
+                parts13 = row[12]
+                all_data.append([parts1, parts2,parts3,parts4,parts5,parts6,parts7,parts8,parts9,parts10,parts11,parts12,parts13]) # Add row to all_data                
     
     # Prepare SQL statement
-    sql = "INSERT INTO players (player_name, player_id) \
-    VALUES (%s, %s)"
-
+    sql = "INSERT INTO stats_match (namePlayer, team, pts, reb, ast, min, fgm, fga, `2pm`, `2pa`, `3pm`, `3pa`, ftm) \
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     # Create arrays to store successes and errors
     success_count = 0
     error_count = 0
@@ -74,16 +83,18 @@ def read_csv_files(csv_files):
     for row in all_data:
         values = tuple(row)
         cursor = db.cursor()
-        
         try:
             cursor.execute(sql, values)
             db.commit()
             success_count += 1
             successes.append(row)
+            print("SQL query:", sql) # Print the SQL query
         except mysql.connector.Error as error:
+            print
             db.rollback()
             error_count += 1
             errors.append(row)
+            print("Error message:", error)
         finally:
             cursor.close()
             
@@ -92,7 +103,7 @@ def read_csv_files(csv_files):
 if __name__ == '__main__':
     create_table()
         
-    csv_files = glob.glob('csv/basketball/players/*.csv')
+    csv_files = glob.glob('csv/basketball/summary/*.csv')
     all_data = read_csv_files(csv_files)    
     # Close the database connection
     db.close()
