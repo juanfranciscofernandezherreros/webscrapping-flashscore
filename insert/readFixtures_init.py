1865ee7ffd0ecddd79108a7ca64c1d8e4474fbfe
Your Name <juanfranciscofernandezherreros>
2023-04-11 17:54:22 +0200

    add fixtures

--- insert/readFixtures_init.py
+++ insert/readFixtures_init.py
@@ -24,14 +24,11 @@ def create_table():
         cursor.execute("""
             CREATE TABLE fixtures (
                 id BIGINT NOT NULL AUTO_INCREMENT,
-                matchId VARCHAR(200),
-                country VARCHAR(200),
-                competition VARCHAR(200),
-                fecha VARCHAR(200),
-                homeTeamLogo VARCHAR(200),
-                visitorTeamLogo VARCHAR(200),
-                homeTeam VARCHAR(200),
-                visitorTeam VARCHAR(200),
+                EventTimeUTC INT,
+                EventTimeUTC VARCHAR(255),
+                homeTeam VARCHAR(255),
+                awayTeam VARCHAR(255),
+                matchId VARCHAR(255),                
                 PRIMARY KEY (id)
             );
         """)
@@ -45,25 +42,20 @@ def read_csv_files(csv_files):
     for file in csv_files:
         print("File: " + file)
         with open(file, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')  # Use semicolon as delimiter
+            reader = csv.reader(csvfile, delimiter=';')  # Use semicolon as delimiter
             next(reader) # Skip header row
             for row in reader:
                 print(row)
-                # Split the row into parts using ',' as a separator
                 parts1 = row[0]
                 parts2 = row[1]
                 parts3 = row[2]
                 parts4 = row[3]
-                parts5 = row[4]
-                parts6 = row[5]
-                parts7 = row[6]
-                parts8 = row[7]
-
-                all_data.append([parts1, parts2, parts3, parts4, parts5,parts6,parts7,parts8]) # Add row to all_data                
+                parts5 = row[4]                
+                all_data.append([parts1, parts2, parts3, parts4, parts5]) # Add row to all_data                
     
     # Prepare SQL statement
-    sql = "INSERT INTO fixtures (matchId, country, competition, fecha, homeTeamLogo, visitorTeamLogo, homeTeam, visitorTeam) \
-    VALUES (%s, %s, %s, %s, %s,%s,%s,%s)"
+    sql = "INSERT INTO fixtures (EventTimeUTC, EventTime, homeTeam, awayTeam, matchId) \
+    VALUES (%s, %s, %s, %s, %s)"
 
     
     # Create arrays to store successes and errors
@@ -74,10 +66,10 @@ def read_csv_files(csv_files):
            
     for row in all_data:
         values = tuple(row)
-        cursor = db.cursor()
-        
+        cursor = db.cursor()        
         try:
             cursor.execute(sql, values)
+            print(sql)
             db.commit()
             success_count += 1
             successes.append(row)
@@ -92,7 +84,7 @@ def read_csv_files(csv_files):
 
 if __name__ == '__main__':
     create_table()
-    csv_files = glob.glob('csv/basketball/fixtures/*.csv')
+    csv_files = glob.glob('../csv/basketball/fixtures/*.csv')
     all_data = read_csv_files(csv_files)
     
     # Close the database connection
