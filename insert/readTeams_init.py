e34c47ef29772fa48c16008d8ad1910c1ab1c88a
Your Name <juanfranciscofernandezherreros>
2023-04-11 16:14:30 +0200

    Teams

--- insert/readTeams_init.py
+++ insert/readTeams_init.py
@@ -10,21 +10,41 @@ db = mysql.connector.connect(
     database="bigdataetl"
 )
 
+def create_table():
+    cursor = db.cursor()
+
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM information_schema.tables
+        WHERE table_name = 'teams'
+    """)
+    table_exists = cursor.fetchone()[0]
+
+    if not table_exists:
+        cursor.execute("""
+            CREATE TABLE teams (
+                id BIGINT NOT NULL AUTO_INCREMENT,
+                team_name VARCHAR(200),
+                team_id VARCHAR(200) UNIQUE,	
+                PRIMARY KEY (id)
+            );
+        """)
+    
+    cursor.close()
+
+
 def read_csv_files(csv_files):
     all_data = []
     
     for file in csv_files:
         print("File: " + file)
         with open(file, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=';')
-            for row in reader:
-                # Split the row into parts using ';' as a separator
-                parts = row[0].split('/')
-
+            reader = csv.reader(csvfile, delimiter=',')
+            next(reader) # Skip header row
+            for row in reader:                
                 # The team name and ID are the 4th and 5th elements of the parts list respectively
-                team_name = parts[4]
-                team_id = parts[5]
-
+                team_name = row[0]
+                team_id = row[1]
                 print("Team Name: ", team_name)
                 print("Team ID: ", team_id)
                 all_data.append((team_name,team_id)) # Add row to all_data                
@@ -61,5 +81,6 @@ def read_csv_files(csv_files):
     db.close()
 
 if __name__ == '__main__':
-    csv_files = glob.glob('csv/basketball/teams/*.csv')
+    create_table()
+    csv_files = glob.glob('../csv/basketball/teams/*.csv')
     all_data = read_csv_files(csv_files)
