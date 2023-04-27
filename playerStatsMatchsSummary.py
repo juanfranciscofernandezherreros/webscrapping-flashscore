44d9ac6e5ce85329b34385118d60041b6c925c91
Your Name <juanfranciscofernandezherreros>
2023-04-27 17:16:35 +0200

    Comprobar los tabs

--- playerStatsMatchsSummary.py
+++ playerStatsMatchsSummary.py
@@ -9,62 +9,56 @@ import sys
 import os
 
 async def main(url):
-
-    # Make sure the "csv" folder exists
-    if not os.path.exists("csv"):
-        os.mkdir("csv")
-
-    # Create a subfolder called "basketball"
-    basketball_folder = os.path.join("csv", "basketball")
-    if not os.path.exists(basketball_folder):
-        os.mkdir(basketball_folder)
-
-    # Create a subfolder called "lineups"
-    urls_folder = os.path.join(basketball_folder, "summary")
-    if not os.path.exists(urls_folder):
-        os.mkdir(urls_folder)
-
     browser = await launch(headless=False)
     page = await browser.newPage()
     await page.goto(url)
 
+    tabs = await page.querySelectorAll('a.tabs__tab')
+
     print("SUMMARY", url)
-    
-    # Obtener el id del partido
-    name_file = url.split("/")[-4]+"_summary"
     game_id = url.split("/")[-4]
 
-    # Seleccionar el elemento usando page.querySelector()
-    total_home = await page.querySelector('.smh__part.smh__score.smh__home.smh__part--current')
-    total_away = await page.querySelector('.smh__part.smh__score.smh__away.smh__part--current')
-
-    # Obtener el texto del elemento usando page.evaluate()
-    total__home = await page.evaluate('(element) => element.textContent', total_home)
-    # Obtener el texto del elemento usando page.evaluate()
-    total__away = await page.evaluate('(element) => element.textContent', total_away)
-
-    quarters = ['1', '2', '3', '4', '5']
-
-    with open(f"csv/basketball/summary/{name_file}.csv", "w", newline='') as f:
-        writer = csv.writer(f)
-
-        # Escribir la primera fila con los encabezados
-        writer.writerow(["Game ID", "Total Home", "Total Away", "Q1 Local", "Q1 Away", "Q2 Local", "Q2 Away", "Q3 Local", "Q3 Away", "Q4 Local", "Q4 Away", "Q5 Local", "Q5 Away"])
-
-        # Escribir la fila con los datos del partido
-        row = [game_id, total__home, total__away]
-        for q in quarters:
-            quarter_local = await page.querySelector(f'.smh__part.smh__home.smh__part--{q}')
-            quarter_away = await page.querySelector(f'.smh__part.smh__away.smh__part--{q}')
-            quarter_local_text = await page.evaluate('(element) => element.textContent', quarter_local)
-            quarter_away_text = await page.evaluate('(element) => element.textContent', quarter_away)
-            row.append(quarter_local_text)
-            row.append(quarter_away_text)
-
-        writer.writerow(row)
+    for tab in tabs:
+        tab_text = await page.evaluate('(element) => element.textContent', tab)
+        print("TABS",tab_text)
+        if tab_text == "Summary":
+            print("Summary")
+            summary_data = await _get_summary_data(page)
+            await _write_summary_to_csv(game_id, summary_data)    
 
     await browser.close()
 
+
+async def _get_summary_data(page):
+    total_home = await page.querySelector('.smh__part.smh__score.smh__home.smh__part--current')
+    total_away = await page.querySelector('.smh__part.smh__score.smh__away.smh__part--current')
+    total__home = await page.evaluate('(element) => element.textContent', total_home)
+    total__away = await page.evaluate('(element) => element.textContent', total_away)
+
+    quarters = ['1', '2', '3', '4', '5']
+    quarter_scores = []
+
+    for q in quarters:
+        quarter_local = await page.querySelector(f'.smh__part.smh__home.smh__part--{q}')
+        quarter_away = await page.querySelector(f'.smh__part.smh__away.smh__part--{q}')
+        quarter_local_text = await page.evaluate('(element) => element.textContent', quarter_local)
+        quarter_away_text = await page.evaluate('(element) => element.textContent', quarter_away)
+        quarter_scores.append(quarter_local_text)
+        quarter_scores.append(quarter_away_text)
+
+    return {
+        "total_home": total__home,
+        "total_away": total__away,
+        "quarter_scores": quarter_scores
+    }
+
+
+async def _write_summary_to_csv(game_id, summary_data):
+    filename = f"csv/basketball/summary/{game_id}_summary.csv"
+    with open(filename, "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Game ID", "Total Home", "Total Away", "Q1 Local", "Q1 Away", "Q2 Local", "Q2 Away", "Q3 Local", "Q3 Away", "Q4 Local", "Q4 Away", "Q5 Local", "Q5 Away"])
+        writer.writerow([game_id, summary_data["total_home"], summary_data["total_away"]] + summary_data["quarter_scores"])
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
