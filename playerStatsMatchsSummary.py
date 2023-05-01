e812e3fb2a230cd6a4d78c40b1b01566c9008412
Your Name <juanfranciscofernandezherreros>
2023-05-01 15:52:37 +0200

    Add match statics

--- playerStatsMatchsSummary.py
+++ playerStatsMatchsSummary.py
@@ -40,7 +40,18 @@ async def main(url):
                 # Get all cell elements and their text
                 data = await _get_player_stats_data(page)
                 await _write_playerStatistics_to_csv(game_id, data)     
-                
+        # Navigate to the player statistics page
+        if tab_text == "Stats":
+                print("Stats")
+                href = await tab.getProperty('href')
+                href_val = await href.jsonValue()
+                url = href_val + "/0"
+                print("Navigating to:", url)
+                await page.goto(url)            
+                await asyncio.sleep(5) 
+                # Get all cell elements and their text
+                data = await _get_match_stats_data(page)
+                await _write_matchStatistics_to_csv(game_id, data)     
     await browser.close()
 
 
@@ -96,6 +107,26 @@ async def _get_player_stats_data(page):
 
     return data
 
+async def _get_match_stats_data(page):
+    # Use page.querySelectorAll to extract all elements with the class 'stat__category'
+    elements = await page.querySelectorAll('.stat__category')
+    data = []
+    
+    # Loop over the elements and extract the text content of each group of elements
+    for element in elements:
+        # Use element.querySelectorAll to extract the three matching elements
+        value_elements = await element.querySelectorAll('.stat__homeValue, .stat__categoryName, .stat__awayValue')
+        
+        # Extract the text content of each element and add it to the data list
+        values = []
+        for value_element in value_elements:
+            value = await page.evaluate('(value_element) => value_element.textContent', value_element)
+            values.append(value)
+        data.append(values)
+    
+    return data
+
+
 async def _write_summary_to_csv(game_id, summary_data):
     filename = f"csv/basketball/summary/{game_id}_summary.csv"
     with open(filename, "w", newline='') as f:
@@ -112,7 +143,14 @@ async def _write_playerStatistics_to_csv(game_id, player_stats_data):
             row.append(game_id)  # Add the Game ID to the row data
             writer.writerow(row)
 
-
+async def _write_matchStatistics_to_csv(game_id, player_stats_data):
+    filename = f"csv/basketball/matchStatistics/{game_id}_matchStatistics.csv"
+    with open(filename, "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Local", "Stats", "Visitor","Game ID"])
+        for row in player_stats_data:
+            row.append(game_id)  # Add the Game ID to the row data
+            writer.writerow(row)
 
 
 if __name__ == '__main__':
